67f809f52b022cbd508ed1ef474abbb5e7e67909
StephenOlusanya <soolusanya@gmail.com>
2022-03-10 20:59:58 -0800

    My commit mesage

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
-/*C program to print "Programming is like building a multilingual puzzle", using the puts function*/
 #include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
-

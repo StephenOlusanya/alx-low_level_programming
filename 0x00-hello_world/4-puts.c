f9a438663a8cc0993c45f05539d701dbfbbf8cc8
StephenOlusanya <soolusanya@gmail.com>
2022-03-10 19:47:29 -0800

    4-puts.c commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
-// C program to print exactly "Programming is like building a multilingual puzzle, followed by a new line.
+// C program to print "Programming is like building a multilingual puzzle", using the puts function
 #include <stdio.h>
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
